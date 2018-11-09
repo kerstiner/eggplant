10b129503a5a9b090842d7970da5afe6f234e849
Kerstine Rodilla <kerstinerodilla@gmail.com>
2018-11-09 14:50:37 +0800

    added jquery

--- js/script.js
+++ js/script.js
@@ -1,35 +1,36 @@
 var mybox = document.getElementById('mybox');
 mybox.onclick = function(){
-	mybox.style.backgroundColor = 'red';
+mybox.style.backgroundColor = 'red';
 }
 
 var box2 = document.getElementById('box2');
 btn1.onclick = function(){
-	box2.style.backgroundColor = 'red';
+box2.style.backgroundColor = 'red';
 }
 
 var box3 = document.getElementById('box3');
 btn2.onclick = function(){
-	box3.style.width = "300px";
-	box3.style.height = "100px";
+box3.style.width = "300px";
+box3.style.height = "100px";
 }
 
 var box4 = document.getElementById('box4');
 texthover.onmouseover = function(){
-	texthover.innerHTML = 'HELLO WORLD';
+texthover.innerHTML = 'HELLO WORLD';
 }
 
 var box5 = document.getElementById('box5');
 movetext.onmouseover = function(){
-	movetext.style.marginTop = "170px";
+movetext.style.marginTop = "170px";
 }
 
 var box6 = document.getElementById('box6');
 inputtxt.oninput = function(){
-	result.innerHTML = inputtxt.value;
+result.innerHTML = inputtxt.value;
 }
 
 var box7 = document.getElementById('box7');
 btn3.onclick = function(){
-	box7.classList.toggle("boxy");
+box7.classList.toggle("boxy");
 }
+
---
+++ js/script1.js
@@ -0,0 +1,39 @@
+function changeColor(color, box) {
+	box.css('background-color', color);
+}
+$(document).ready(function(){
+
+	$("#mybox").on('click', function(){
+		changeColor('red', $(this))
+	});
+
+	$("#btn1").on('click', function(){
+		changeColor('green', $("#box2"))
+	});
+
+	var btn2 = $("#btn2");
+
+	btn2.on('click', function(){
+		$("#box3").width("300px");
+		$("#box3").height("100px");
+	});
+
+	$("#texthover").on('mouseover', function(){
+		$(this).html("HELLO WORLD");
+	});
+
+	$("#movetext").on('mouseover', function(){
+		$(this).css("margin-top", "170px");
+	});
+
+	var result = $("#result");
+
+	$("#inputtxt").on('keyup', function(){
+		var value = $(this).val();
+		result.text(value);
+	});
+
+	$("#btn3").on('click', function(){
+		$("#box7").toggleClass("boxy");
+	});
+});
